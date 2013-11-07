ae739b0911477efa41644ed14e01488d361f46ac
Cvazer <zhirnitoni@gmail.com>
2013-11-07 22:43:56 +0200

    Update Main.java

--- HelloWorld(Git-Test)/HelloWorld/src/Main.java
+++ HelloWorld(Git-Test)/HelloWorld/src/Main.java
@@ -1,9 +1,9 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		//System.out.println("HelloWorld");
-		System.out.println("i'v just realised, that i dont need previous code anymore!");
+		//System.out.println("i'v just realised, that i dont need previous code anymore!");
+		System.out.println("and again i screw everything up!");
 	}
 
 }

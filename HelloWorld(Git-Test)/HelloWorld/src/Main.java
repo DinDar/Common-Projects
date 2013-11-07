0dbe7f7db1e733f89260175b3722a7d5452a5eac
Cvazer <zhirnitoni@gmail.com>
2013-11-07 23:59:38 +0300

    Now all just work fine )

--- HelloWorld(Git-Test)/HelloWorld/src/Main.java
+++ HelloWorld(Git-Test)/HelloWorld/src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//System.out.println("HelloWorld");
-		//System.out.println("i'v just realised, that i dont need previous code anymore!");
-		System.out.println("and again i screw everything up!");
+		System.out.println("HelloWorld");
 	}
 
 }
